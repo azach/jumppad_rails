71926dfe00716040ee8b7ea30a504501f6ad8326
anthony <anthonyzacharakis@gmail.com>
2012-01-19 23:31:51 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+source 'http://gems.github.com'
+
+gem 'mysql2'
+gem 'rails', '3.1'
+gem 'rubygems-update'
+
+group :development do
+end
+
+group :test do
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JumppadRails::Application.config.secret_token = 'fd478b9cfee030954d588198aec85638241e31fca7eddbc794cfaae32fe2c29baff234ea002401be149e1337d3b4b6924ce563f48b73edee552d4122a55af9bb'

700d8b965e68b3aa93b414efb3ccab34d1de0b13
anthony <anthonyzacharakis@gmail.com>
2012-01-22 12:37:07 -0800

    Better page titles; use HTML entities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def page_title(content_for_title = nil)
+	  @title = (content_for_title + " #{HTMLEntities.new.decode('&mdash;')} " if content_for_title).to_s + 'JumpPad'
+	end
 end

8c4e5c77e8343fd806cd7bcc115822dfcdde8e21
anthony <anthonyzacharakis@gmail.com>
2012-01-22 11:43:18 -0800

    Check for nil end date

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,6 @@ class Trip < ActiveRecord::Base
 	validate :start_date_must_be_before_end_date
 	
 	def start_date_must_be_before_end_date
-		errors.add(:end_date, "can't be before the start date") unless end_date > start_date || start_date.nil?
+		errors.add(:end_date, "can't be before the start date") unless end_date.nil? || start_date.nil? || end_date > start_date
 	end
 end
